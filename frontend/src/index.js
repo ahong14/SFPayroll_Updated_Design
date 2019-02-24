78091323f1daac2f14859709ac74854d0a3686dc
Alex Hong <ahong@ucdavis.edu>
2019-02-23 17:57:02 -0800

    attempting refresh fix with module.hot

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,10 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+// hot module reloading used to reload app in browser w/out performing a page
+// refresh. Useful when wanting to test w/out losing console.log() s
+if (module.hot) {
+  module.hot.accept()
+}
+
 serviceWorker.unregister();
