b7f9ce62902a806414387ace6e1650459fad0468
Alex Hong <ahong@ucdavis.edu>
2019-03-25 16:17:07 -0700

    added new PO box to footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -11,6 +11,7 @@ class Footer extends Component{
             <div id = "ending">
                 <div id = "ending_content">
                     <p> San Francisco Bay Area Chapter </p>
+                    <p> PO Box: PO Box 683. San Bruno, CA 94066 </p>
                     <a id = "apa_link" href = "https://www.americanpayroll.org/" target = "_blank"> <p> American Payroll Association </p> </a>
                     <Grid container direction = "row"  justify = "flex-start">
                       <a className = "footerLink" href = "https://www.linkedin.com/in/sfbac-apa-74bb7421/" target = "_blank"> <FaLinkedin/> </a>
