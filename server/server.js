58513effc356490a5b024e136fcd2106b7dbd872
Alex Hong <alexhong681@gmail.com>
2019-12-24 22:56:28 -0800

    setup docker compose for dev, added overflow scroll for job postings and events

--- server/server.js
+++ server/server.js
@@ -12,58 +12,6 @@ mongoose.connection.on('error', function(error) {
     console.error('Database connection error:', error);
 });
 
-//TODO: remove heroku dependencies
-
-//code sourced from https://spin.atomicobject.com/2018/05/15/extending-heroku-timeout-node/
-//code to workaround heroku deployment 30second timeout
-//sending emails takes longer than 30 seconds, heroku will timeout and return 503 service unavailable
-
-const extendTimeoutMiddleware = (req, resp, next) => {
-    const space = ' ';
-    let isFinished = false;
-    let isDataSent = false;
-
-    resp.once('finish', () => {
-      isFinished = true;
-    });
-  
-    resp.once('end', () => {
-      isFinished = true;
-    });
-  
-    resp.once('close', () => {
-      isFinished = true;
-    });
-  
-    resp.on('data', (data) => {
-      // Look for something other than our blank space to indicate that real
-      // data is now being sent back to the client.
-      if (data !== space) {
-        isDataSent = true;
-      }
-    });
-  
-    const waitAndSend = () => {
-      setTimeout(() => {
-        // If the response hasn't finished and hasn't sent any data back....
-        if (!isFinished && !isDataSent) {
-          // Need to write the status code/headers if they haven't been sent yet.
-          if (!resp.headersSent) {
-            resp.writeHead(202);
-          }
-  
-          resp.write(space);
-  
-          // Wait another 15 seconds
-          waitAndSend();
-        }
-      }, 15000);
-    };
-  
-    waitAndSend();
-    next();
-};
-  
 //application, running express app
 const app = express();
 
@@ -75,9 +23,6 @@ app.use(cors());
 
 //serve react files
 app.use(express.static(path.join(__dirname, '/../frontend/build')));
-
-//use middleware
-app.use(extendTimeoutMiddleware);
 
 //routes
 const events = require('./routes/events');
@@ -92,10 +37,13 @@ app.use('/api/job', job);
 app.use('/api/positions', positions);
 app.use('/api/pdfs', pdfs);
 
+//check if dev or production mode
 //fix react app crashing on refresh
-app.get('/*', (req,res) => {
-  res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
-})
+if(process.env.production){
+  app.get('/*', (req,res) => {
+    res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+  })
+}
 
 //listen to requests on port
 //choose port based on environment
