f84787135229b0244467cb2002687c39fe5c4f6a
Alex Hong <alexhong681@gmail.com>
2020-01-31 22:14:26 -0800

    removed norton member from officers list

--- frontend/src/components/AboutUs/Members/Members.js
+++ frontend/src/components/AboutUs/Members/Members.js
@@ -34,9 +34,6 @@ class Members extends Component{
                                          email = 'sfbac.apa@gmail.com'/>
                   <OfficerMember imagesrc= {lois} name = 'Lois Fried, CPP' position = 'Historian' company = "SFBAC" 
                                          email = 'sfbac.apa@gmail.com'/>
-                                        
-                  <OfficerMember imagesrc = {kim} name = 'Kim Norton, CPP' position = 'Bulletin Chairperson' company = "SFBAC"
-                                         email = 'sfbac.apa@gmail.com'/>
                   <OfficerMember imagesrc = {alex} name = 'Alex Hong' position = 'Web Developer Intern' email = 'alexhong681@gmail.com'/>
                   <OfficerMember imagesrc = {noImage} name = 'Catherine Zolli' position = 'Hospitality Chairperson' email = 'sfbac.apa@gmail.com'/>
                   <EmptyMember/>
