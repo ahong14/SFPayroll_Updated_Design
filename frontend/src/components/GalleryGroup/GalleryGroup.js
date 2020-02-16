4e49050e894fe2cbbc78e6c7c559253ac8fba4e5
Alex Hong <alexhong681@gmail.com>
2020-02-16 15:23:39 -0800

    added library to lazy load images, moved EXIF logic to afterLoad function of LazyLoadImage component

--- frontend/src/components/GalleryGroup/GalleryGroup.js
+++ frontend/src/components/GalleryGroup/GalleryGroup.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import GalleryItem from '../GalleryItem/GalleryItem';
 import axios from 'axios';
 import './GalleryGroup.css';
-import EXIF from 'exif-js';
 
 class GalleryGroup extends Component{
     constructor(props){
@@ -28,39 +27,6 @@ class GalleryGroup extends Component{
             .catch(err => {
                 alert(err.response.data.message);
             })
-    }
-
-    //wait for images to return
-    //check EXIF Orientation values for each image
-    //if orientation value != 1, image was rotated, need to rotate back
-    componentDidUpdate(prevProps, prevState){
-        if(prevState.imageSources.length == 0 && this.state.imageSources.length > 0){
-            //get all images and check orientation value for each image
-            let imageElements = document.getElementsByClassName("galleryImage");
-            imageElements =  [...imageElements];
-            imageElements.forEach(element => {
-                //reference: https://stackoverflow.com/questions/59703859/how-to-use-exif-js-in-a-react-component
-                //when image loads async, check orientation tag value
-                element.onload = () => {
-                    EXIF.getData(element, () => {
-                        //reference: https://sirv.com/help/resources/rotate-photos-to-be-upright/
-                        //exif orientation ranges from 1 to 8
-                        let exifOrientationValue = EXIF.getTag(element, 'Orientation');
-                        //check for rotated images, set back to 1
-                        switch(exifOrientationValue){
-                            case 3:
-                                element.style.transform = "rotate(180deg)";
-                                break;
-                            case 6:
-                                element.style.transform = "rotate(90deg)";
-                                break;
-                            default:
-                                break;
-                        }
-                    })
-                }
-            });
-        }
     }
 
     render(){
--- frontend/src/components/GalleryItem/GalleryItem.js
+++ frontend/src/components/GalleryItem/GalleryItem.js
@@ -1,26 +1,61 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { LazyLoadImage, trackWindowScroll } from 'react-lazy-load-image-component';
+import EXIF from 'exif-js';
 import './GalleryItem.css';
 
 class GalleryItem extends Component {
   constructor(props){
     super(props);
+    this.imageRef = React.createRef()
+  }
+
+  //function to call after image lazily loaded
+  //check if EXIF orientation value caused image to rotate
+  //use css property to re-rotate based on orientation tag value
+  checkOrientationValue = () => {
+    //find ReactDOM node of current ref
+    //get child node which contains img element
+    let currentNode = ReactDOM.findDOMNode(this.imageRef.current);
+    let imageNode = currentNode.childNodes[0];
+    //get EXIF data of img element (Exchangeable Image File)
+    EXIF.getData(imageNode, () => {
+      //reference: https://sirv.com/help/resources/rotate-photos-to-be-upright/
+      //exif orientation ranges from 1 to 8
+      let exifOrientationValue = EXIF.getTag(imageNode, 'Orientation');
+      //check for rotated images, set back to 1
+      switch(exifOrientationValue){
+          case 3:
+              imageNode.style.transform = "rotate(180deg)";
+              break;
+          case 6:
+              imageNode.style.transform = "rotate(90deg)";
+              break;
+          default:
+              break;
+      }
+    })
   }
 
   render() {
     return (
       <div className="col-lg-4">
-        <div className="thumbnail">
-          <a href={this.props.imageSrc} rel="noopener noreferrer" target="_blank">
-            <img className="galleryImage img-fluid" src={this.props.imageSrc} alt="Gallery Image"/>
-            <div className="caption">
-              <p className="text-center"> <strong> {this.props.caption} </strong> </p>
-            </div>
-            
-          </a>
-        </div>
+        <a href={this.props.imageSrc} rel="noopener noreferrer" target="_blank"> 
+          <LazyLoadImage
+            className="galleryImage"
+            alt="gallery"
+            src={this.props.imageSrc}
+            effect="blur"
+            ref={this.imageRef}
+            afterLoad={this.checkOrientationValue}
+            scrollPosition={this.props.scrollPosition}
+          />
+        </a>
       </div>
     )
   }
 }
 
-export default GalleryItem;
+//HOC to pass scroll position as prop
+//reference: https://www.npmjs.com/package/react-lazy-load-image-component
+export default trackWindowScroll(GalleryItem);
