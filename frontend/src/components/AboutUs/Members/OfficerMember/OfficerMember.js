0e40de29c61b2d675b3a864d9716bb9ae09edf7a
Alex Hong <alexhong681@gmail.com>
2021-02-14 22:03:21 -0800

    updated homepage apa and cpc images, updated member image

--- frontend/src/components/AboutUs/Members/OfficerMember/OfficerMember.js
+++ frontend/src/components/AboutUs/Members/OfficerMember/OfficerMember.js
@@ -1,25 +1,38 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import '../../../AboutUs/Members/OfficerMember/OfficerMember.css';
 
-class OfficerMember extends Component{
-    render(){
-        return(
-          <div className="col-md-3">
-            <div className="card memberCard">
-                <img className="card-img-top memberImage" src={this.props.imagesrc} alt="Member"/>
-                <div className="card-body">
-                    <h5 className="card-title text-center font-weight-bold"> {this.props.name} </h5>
-                    <p className="card-text text-center font-weight-bold"> {this.props.position} </p>
-                    <p className="card-text text-center font-weight-bold"> {this.props.company} </p>
+class OfficerMember extends Component {
+    render() {
+        return (
+            <div className="col-md-3">
+                <div className="card memberCard">
+                    <img
+                        className="card-img-top memberImage"
+                        src={this.props.imagesrc}
+                        alt="Member"
+                    />
+                    <div className="card-body">
+                        <h5 className="card-title text-center font-weight-bold">
+                            {' '}
+                            {this.props.name}{' '}
+                        </h5>
+                        <p className="card-text text-center font-weight-bold">
+                            {' '}
+                            {this.props.position}{' '}
+                        </p>
+                        <p className="card-text text-center font-weight-bold">
+                            {' '}
+                            {this.props.company}{' '}
+                        </p>
+                    </div>
                 </div>
             </div>
-          </div>
         );
     }
 }
 
 OfficerMember.defaultProps = {
-  imagesrc: '//:0'
-}
+    imagesrc: '//:0'
+};
 
 export default OfficerMember;
